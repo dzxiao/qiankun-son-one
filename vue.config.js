b0725618d070e96a2b12daeb96e3aa3e97af1e8e
dengzixiao <dzxXiao1111@163.com>
2023-01-30 15:33:03 +0800

    feat: configureQK

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const { name } = require('./package');
+module.exports = {
+  devServer: {
+    port:81,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+  },
+  configureWebpack: {
+    output: {
+      library: `${name}-[name]`,
+      libraryTarget: 'umd', // 把微应用打包成 umd 库格式
+      chunkLoadingGlobal: `webpackJsonp_${name}`,
+    },
+  },
+};
